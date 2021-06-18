e19af09c20d69ae5227363a458af41b240c26cc5
Onur <onurr.yazici@outlook.com>
2021-06-18 18:56:06 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+import * as THREE from "three"
 
-export default App;
+class App extends Component {
+    componentDidMount() {
+      	var scene 	   = new THREE.Scene()
+      	var camera 	   = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 ) // For displaying camera
+      	var renderer   = new THREE.WebGLRenderer()
+      	var geometry   = new THREE.BoxGeometry( 10, 10, 10 ) // Cube with x, y, z property 
+      	var material   = new THREE.MeshBasicMaterial( { color: 0x0066cc } ) // Cube color
+      	var cube	   = new THREE.Mesh( geometry, material ) // Cube object
+		var gridHelper = new THREE.GridHelper( 150, 15 ); // Grid for scene
+		
+		camera.position.x = 3
+		camera.position.y = 10
+		camera.position.z = 35
+		
+		gridHelper.colorGrid = 0xdedede
+		renderer.setSize( window.innerWidth , window.innerHeight -1 )
+		this.mount.appendChild(renderer.domElement)
+      	
+		scene.add(cube)
+		scene.add( gridHelper );
+		
+      	var animate = function () {
+      	  	requestAnimationFrame( animate )
+      	  	cube.rotation.y += 0.05
+      	  	renderer.render( scene, camera )
+      	}
+      	animate()
+    }
+    render() {
+      	return (
+			<div ref={ref => (this.mount = ref)} />
+      	)
+    }
+  }
+  const rootElement = document.getElementById("root")
+  ReactDOM.render(<App />, rootElement)
+export default App
